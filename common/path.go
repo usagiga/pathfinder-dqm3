d50926792830cb7a092ec6182389196a5afd99b3
usagiga <usagiga@usagiga.me>
2024-01-08 06:55:28 +0900

    fix: redefine synth path

--- common/path.go
+++ common/path.go
@@ -1,44 +1,83 @@
 package common
 
-// PathBase 配合ツリーのノード。各ノードには配合対象となるモンスターが記述される
-type PathBase struct {
+type SynthPath interface {
+	GetMonsterId() int
+	GetSynthPathType() SynthPathType
+}
+
+type SynthPathType int
+
+const (
+	SynthPathType_Root SynthPathType = iota + 1
+	SynthPathType_NominalSynth
+	SynthPathType_SpecialSynth
+	SynthPathType_SpecialSynthWithRank
+	SynthPathType_QuadSynth
+)
+
+// SynthPathBase 配合ツリーのノード。各ノードには配合対象となるモンスターが記述される
+type SynthPathBase struct {
 	MonsterId int
 }
 
-// PathRoot スカウト限定、配信限定、タマゴ限定など
-type PathRoot struct {
-	*PathBase
+func (p *SynthPathBase) GetMonsterId() int {
+	return p.MonsterId
 }
 
-// PathSynth 通常配合
-type PathSynth struct {
-	*PathBase
+// SynthPathRoot スカウト限定、配信限定、タマゴ限定など
+type SynthPathRoot struct {
+	*SynthPathBase
+}
+
+func (p *SynthPathRoot) GetSynthPathType() SynthPathType {
+	return SynthPathType_Root
+}
+
+// SynthPathNominalSynth 通常配合
+type SynthPathNominalSynth struct {
+	*SynthPathBase
 	XFamilyId    Family
 	XRank        Rank
 	YMonsterId   Family
 	YMonsterRank Rank
 }
 
-// PathSpecialSynth 特殊配合
-type PathSpecialSynth struct {
-	*PathBase
+func (p *SynthPathNominalSynth) GetSynthPathType() SynthPathType {
+	return SynthPathType_NominalSynth
+}
+
+// SynthPathSpecialSynth 特殊配合
+type SynthPathSpecialSynth struct {
+	*SynthPathBase
 	XMonsterId int
 	YMonsterId int
 }
 
-// PathSpecialSynthWithRank 特殊配合(片方が系統・ランクによる配合)
-type PathSpecialSynthWithRank struct {
-	*PathBase
+func (p *SynthPathSpecialSynth) GetSynthPathType() SynthPathType {
+	return SynthPathType_SpecialSynth
+}
+
+// SynthPathSpecialSynthWithRank 特殊配合(片方が系統・ランクによる配合)
+type SynthPathSpecialSynthWithRank struct {
+	*SynthPathBase
 	XMonsterId int
 	YFamily    Family
 	YRank      Rank
 }
 
-// PathQuadSynth 4体配合
-type PathQuadSynth struct {
-	*PathBase
+func (p *SynthPathSpecialSynthWithRank) GetSynthPathType() SynthPathType {
+	return SynthPathType_SpecialSynthWithRank
+}
+
+// SynthPathQuadSynth 4体配合
+type SynthPathQuadSynth struct {
+	*SynthPathBase
 	AMonsterId int
 	BMonsterId int
 	CMonsterId int
 	DMonsterId int
+}
+
+func (p *SynthPathQuadSynth) GetSynthPathType() SynthPathType {
+	return SynthPathType_QuadSynth
 }
